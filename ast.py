fb4723625897b9f8793e210c8f68355e2ca51ce5
Chris Timperley <christimperley@googlemail.com>
2016-08-26 01:29:22 +0100

    added more node classes

--- ast.py
+++ ast.py
@@ -1,13 +1,21 @@
 #!/bin/usr/env python3
+#
+# Ignores CppTop for now; easy enough to add in later.
+#
 import json
 
 class Node(object):
+    def __init__(self, pos):
+        self.pos = pos
     def to_s(self):
         raise NotImplementedError('No `to_s` method exists for this object')
     def to_json(self):
         raise NotImplementedError('No `to_json` method exists for this object')
 
-class IfElse(Node):
+class Statement(Node):
+    pass
+
+class IfElse(Statement):
     @staticmethod
     def from_json(jsn):
        pass 
@@ -18,7 +26,14 @@ class IfElse(Node):
     def to_json(self):
         pass
 
-class Return(Node):
+class Constant(Node):
+    def __init__(self, pos, value):
+        super().__init__(pos)
+        self.value = value
+    def to_s(self):
+        return str(self.value)
+
+class Return(Statement):
     pass
 
 class Program(Node):
