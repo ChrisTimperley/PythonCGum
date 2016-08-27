8b280d2088a2d3594df437c13f4bfa4846d02abf
Chris Timperley <christimperley@googlemail.com>
2016-08-27 21:20:34 +0100

    updated preprocessor module

--- preprocessor.py
+++ preprocessor.py
@@ -5,32 +5,28 @@ class PreprocessorStatement(Node):
     CODE = "450300"
     LABEL = "CppTop"
 
-    @staticmethod
-    def from_json(jsn):
-        Node.check_code(jsn['type'], PreprocessorStatement.CODE)
-        return PreprocessorStatement(jsn['pos'],\
-                                     OtherDirective.from_json(jsn['children'][0]))
+    def __init__(self, pos, length, label, children):
+        assert label is None
+        assert len(children) == 1
+        assert isinstance(children[0], OtherDirective)
+        super().__init__(pos, length, label, children)
 
-    def __init__(self, pos, statement):
-        super().__init__(pos)
-        self.__statement = statement
-
+    def statement(self):
+        return self.__children[0]
     def to_s(self):
-        return self.__statement.to_s()
+        return self.statement().to_s()
 
 class OtherDirective(Node):
     CODE = "400400"
     LABEL = "OtherDirective"
 
-    @staticmethod
-    def from_json(jsn):
-        Node.check_code(jsn['type'], OtherDirective.CODE)
-        return OtherDirective(jsn['pos'],\
-                              GenericString.from_json(jsn['children'][0]))
-    
-    def __init__(self, pos, directive):
-        super().__init__(pos)
-        self.__directive = directive
+    def __init__(self, pos, length, label, children):
+        assert label is None
+        assert len(children) == 1
+        assert isinstance(children[0], GenericString)
+        super().__init__(pos, length, label, children)
 
+    def directive(self):
+        return self.__children[0].to_s()
     def to_s(self):
-        return self.__directive.to_s()
+        return self.directive()
