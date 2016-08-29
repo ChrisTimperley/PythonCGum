478d35fe7eb3c0fa5845214ff6d45ab870115b5a
ChrisTimperley <christimperley@googlemail.com>
2016-08-29 01:40:31 +0100

    created easy_setup file

--- cgum/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-import os
-
-SCRIPT_DIR 
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cgum',
+    version='0.0.1',
+    description='CGum AST loader',
+    long_description='TBA',
+    author='Chris Timperley',
+    author_email='christimperley@googlemail.com',
+    url='https://github.com/ChrisTimperley/PythonCGum',
+    license='hello',
+    packages=find_packages()
+)
