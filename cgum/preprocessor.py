7d822412aa5e628e45973cd6de25217e02ec560b
Chris Timperley <christimperley@googlemail.com>
2016-12-15 23:54:42 -0500

    added IfDefTop node type

--- cgum/preprocessor.py
+++ cgum/preprocessor.py
@@ -30,6 +30,21 @@ class OtherDirective(Node):
     def to_s(self):
         return self.directive()
 
+class IfDefTop(Node):
+    CODE = "450400"
+    LABEL = "IfdefTop"
+
+    def __init__(self, pos, length, label, children):
+        assert label is None
+        assert len(children) == 1
+        assert isinstance(children[0], IfDefDirective)
+        super().__init__(pos, length, label, children)
+
+    def directive(self):
+        return self.__children[0].to_s()
+    def to_s(self):
+        return self.directive()
+
 class Include(Node):
     CODE = "400200"
     LABEL = "Include"
