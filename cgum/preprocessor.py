41d7dd6c47f2d5a82ef21ad44d46868437995a94
Chris Timperley <christimperley@googlemail.com>
2016-12-15 23:57:56 -0500

    added ifdefdirective

--- cgum/preprocessor.py
+++ cgum/preprocessor.py
@@ -45,6 +45,21 @@ class IfDefTop(Node):
     def to_s(self):
         return self.directive()
 
+class IfDefDirective(Node):
+    CODE = "440100"
+    LABEL = "IfdefDirective"
+
+    def __init__(self, pos, length, label, children):
+        assert label is None
+        assert len(children) == 1
+        assert isinstance(children[0], GenericString)
+        super().__init__(pos, length, label, children)
+
+    def directive(self):
+        return self.__children[0].to_s()
+    def to_s(self):
+        return self.directive()
+
 class Include(Node):
     CODE = "400200"
     LABEL = "Include"
